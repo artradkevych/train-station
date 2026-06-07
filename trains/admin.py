1fa81a4dc1ab4c8e65dd24b4cdaf36521aafe83e
Artem <artemradckevich@gmail.com>
2026-06-07 12:53:47 +0300

    Registered TrainType and Train models in Django admin

--- trains/admin.py
+++ trains/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from trains.models import TrainType, Train
+
+
+@admin.register(TrainType)
+class TrainTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    list_display = ("name", "cargo_num", "places_in_cargo", "train_type")
+    list_filter = ("train_type",)
+    search_fields = ("name", "train_type__name")
+    select_related = ("train_type",)
