86e61720ee5a7250f3e6a4ff0aa9f2752e7a25ac
Artem <artemradckevich@gmail.com>
2026-06-07 17:11:17 +0300

    Enabled DRF session authentication for browsable API support

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
     path("api/users/", include("users.urls", namespace="users")),
     path("api/fleet/", include("trains.urls", namespace="trains")),
 ]
